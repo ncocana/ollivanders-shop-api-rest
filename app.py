9b4d4b04f5d4d7c90a5c2b732401205dda3e013d
ncocana <ncocana@cifpfbmoll.eu>
2023-03-20 17:20:05 +0100

    feat(app.py, inventory.html): add the object class to the dictionary

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 INVENTORY = {
-    1: {'name':'+5 Dexterity Vest', 'sell_in': 10, 'quality': 20},
-    2: {'name':'Aged Brie', 'sell_in': 2, 'quality': 0},
-    3: {'name':'Elixir of the Mongoose', 'sell_in': 5, 'quality': 7},
-    4: {'name':'Sulfuras, Hand of Ragnaros', 'sell_in': 0, 'quality': 80},
-    5: {'name':'Sulfuras, Hand of Ragnaros', 'sell_in': -1, 'quality': 80},
-    6: {'name':'Backstage passes to a TAFKAL80ETC concert', 'sell_in': 15, 'quality': 20},
-    7: {'name':'Backstage passes to a TAFKAL80ETC concert', 'sell_in': 10, 'quality': 49},
-    8: {'name':'Backstage passes to a TAFKAL80ETC concert', 'sell_in': 5, 'quality': 49},
-    9: {'name':'Conjured Mana Cake', 'sell_in': 3, 'quality': 6}
+    1: {'name':'+5 Dexterity Vest', 'sell_in': 10, 'quality': 20, 'class': 'ConjuredItem'},
+    2: {'name':'Aged Brie', 'sell_in': 2, 'quality': 0, 'class': 'AgedBrie'},
+    3: {'name':'Elixir of the Mongoose', 'sell_in': 5, 'quality': 7, 'class': 'NormalItem'},
+    4: {'name':'Sulfuras, Hand of Ragnaros', 'sell_in': 0, 'quality': 80, 'class': 'Sulfuras'},
+    5: {'name':'Sulfuras, Hand of Ragnaros', 'sell_in': -1, 'quality': 80, 'class': 'Sulfuras'},
+    6: {'name':'Backstage passes to a TAFKAL80ETC concert', 'sell_in': 15, 'quality': 20, 'class': 'Backstage'},
+    7: {'name':'Backstage passes to a TAFKAL80ETC concert', 'sell_in': 10, 'quality': 49, 'class': 'Backstage'},
+    8: {'name':'Backstage passes to a TAFKAL80ETC concert', 'sell_in': 5, 'quality': 49, 'class': 'Backstage'},
+    9: {'name':'Conjured Mana Cake', 'sell_in': 3, 'quality': 6, 'class': 'ConjuredItem'}
 }
 
 @app.route('/')
