797d63237c83ad9bb9b8cc616fec3fc811abb0d7
ncocana <ncocana@cifpfbmoll.eu>
2023-03-30 21:14:36 +0200

    test(test_app): add tests for get item by id and endpoint

--- test_app.py
+++ test_app.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 from app import app
 from database import db
@@ -49,6 +50,36 @@ def test_get_inventory_success(client):
     assert b"Inventory updated" in response.data
 
 
+@pytest.mark.test_endpoints_get
+def test_get_item_with_valid_id(client):
+    response = client.get("/inventory/get?id=7")
+    assert response.status_code == 200
+    assert response.data is not None
+    item = db.get_item_by_id(7)
+    assert json.loads(response.data) == dict(item)
+
+
+@pytest.mark.test_endpoints_get
+def test_get_item_with_missing_id(client):
+    response = client.get("/inventory/get")
+    assert response.status_code == 400
+    assert json.loads(response.data) == {"error": "ID parameter missing"}
+
+
+@pytest.mark.test_endpoints_get
+def test_get_item_with_invalid_id(client):
+    response = client.get("/inventory/get?id=invalid_id")
+    assert response.status_code == 400
+    assert json.loads(response.data) == {"error": "Invalid ID parameter"}
+
+
+@pytest.mark.test_endpoints_get
+def test_get_item_not_found(client):
+    response = client.get("/inventory/get?id=100")
+    assert response.status_code == 404
+    assert json.loads(response.data) == {"error": "Item not found"}
+
+
 # TEST "POST" METHOD.
 
 
@@ -87,7 +118,7 @@ def test_put_inventory(client):
     response = client.put("/inventory")
     assert response.status_code == 200
     assert response.data is not None
-    assert b'{"success":true}' in response.data
+    assert json.loads(response.data) == {"success": True}
 
 
 # TEST "DELETE" METHOD.
